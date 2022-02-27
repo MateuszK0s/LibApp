392e825f23a1851e9ec347ac4b1e4cd8e31bcbe6
MateuszK0s <kosmateusz99@gmail.com>
2022-02-27 12:18:46 +0100

    Task 2A

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,25 +6,36 @@ using System.Threading.Tasks;
 
 namespace LibApp.Models
 {
-    public class Book
-    {
-        public int Id { get; set; }
-		[Required]
+	public class Book
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Name is required.")]
 		[StringLength(255)]
 		public string Name { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Author name is required.")]
 		public string AuthorName { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Genre is required.")]
 		public Genre Genre { get; set; }
-		[Required]
+
+		[Required(ErrorMessage = "Genre Id is required.")]
 		[Display(Name = "Genre")]
 		public byte GenreId { get; set; }
+
 		public DateTime DateAdded { get; set; }
-		[Display(Name="Realease Date")]
+
+		[Required(ErrorMessage = "Release Date is required.")]
+		[Display(Name = "Realease Date")]
 		public DateTime ReleaseDate { get; set; }
+
+		[Required(ErrorMessage = "Number in stock is required.")]
 		public int NumberInStock { get; set; }
+
+		[Required(ErrorMessage = "Number available is required.")]
 		public int NumberAvailable { get; set; }
 	}
+}
       
 }
