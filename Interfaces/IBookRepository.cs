5030eb65c4faa0916d9589d1f49b86b4aa000c92
MateuszK0s <kosmateusz99@gmail.com>
2022-02-27 12:35:30 +0100

    Interface update

--- Interfaces/IBookRepository.cs
+++ Interfaces/IBookRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace LibApp.Interfaces
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
         IEnumerable<Book> GetBooks();
 
